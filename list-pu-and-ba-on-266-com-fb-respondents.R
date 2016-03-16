67dfa1b10349dc868b7927f698c432fab9b011fb
Aditya Khanna <khanna7@uchicago.edu>
2016-03-16 10:41:24 -0500

    list PU's and BA's on common respondents

---
+++ list-pu-and-ba-on-266-com-fb-respondents.R
@@ -0,0 +1,49 @@
+## descriptives on common 266 respondents
+
+  rm(list=ls())
+
+  library(igraph)
+  library(intergraph)
+
+  load(file="data-on-266-com-fbrespondents-btwn-w1-w2.RData")
+
+  ## Check networks of common respondents at w1 and w2
+  w1.com.resp.w.w2.data.ig <- asIgraph(w1.com.resp.w.w2.data.net)
+  w2.com.resp.ig <- asIgraph(w2.com.resp.net)
+
+  w1.com.resp.w.w2.data.ig
+  w2.com.resp.ig
+
+  ## Check if the ordering is consistent
+  identical(V(w1.com.resp.w.w2.data.ig)$vertex.names, V(w2.com.resp.ig)$vertex.names)
+
+  ## Check # and % of PrEP aware in each wave
+  table(V(w1.com.resp.w.w2.data.ig)$prepknow, exclude=NULL)
+  table(V(w2.com.resp.ig)$prepknow, exclude=NULL)
+
+  table(V(w1.com.resp.w.w2.data.ig)$prepknow, exclude=NULL)/
+      sum(table(V(w1.com.resp.w.w2.data.ig)$prepknow, exclude=NULL))
+  table(V(w2.com.resp.ig)$prepknow, exclude=NULL)/
+      sum(table(V(w2.com.resp.ig)$prepknow, exclude=NULL))
+
+  ## List PU and BA
+  pu.id <- intersect(which(V(w1.com.resp.w.w2.data.ig)$prepknow == 0),
+                     which(V(w2.com.resp.ig)$prepknow == 0)
+                     )
+  pu.names <- (V(w1.com.resp.w.w2.data.ig)$vertex.names)[pu.id]
+
+  ba.id <- intersect(which(V(w1.com.resp.w.w2.data.ig)$prepknow == 0),
+                     which(V(w2.com.resp.ig)$prepknow == 1)
+                     )
+  ba.names <- (V(w1.com.resp.w.w2.data.ig)$vertex.names)[ba.id]
+
+  ## num and % of PU and BA
+  length(pu.names)
+  length(ba.names)
+
+  length(pu.names)/vcount(w2.com.resp.ig)
+  length(ba.names)/vcount(w2.com.resp.ig)
+
+  ## compute characteristics
+  
+  save.image(file="pu-vs-ba-correct.RData")
