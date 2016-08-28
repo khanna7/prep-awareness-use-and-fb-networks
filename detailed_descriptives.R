b48491410d3729cd834a06e433eab1f752e4339e
Aditya Khanna <khanna7@uchicago.edu>
2016-08-28 11:29:00 -0500

    add BA summary computations

--- detailed_descriptives.R
+++ detailed_descriptives.R
@@ -85,16 +85,98 @@ dim(ego_data_ba_only)
 
   ## Month of interview data
      pu.month.of.intvw              <- ego_data_pu_only$int.date.month.new
+
+   ## BA only
+
+   ## ses
+   ba.ses.data                    <- subset(ego_data_ba_only, 
+                                        select=
+                                            c(agecalc.cat,
+                                              income.new,
+                                              education.new,
+                                              jailever,
+                                              healthcov,
+                                              homeless12mo,
+                                              employed.recode
+                                              )
+                                        )
+
+   ## health
+   ba.health.data                 <- subset(ego_data_ba_only,
+                                        select=c(hivtest,
+                                            primarycare,
+                                            mentalcare,
+                                            oneonone,
+                                        #hivvac,
+                                            hiveverdiag,
+                                            #stdsyph,
+                                            hivprogever,
+                                            anustest,
+                                            depressed.ind,
+                                            labsyphresult
+                                                 )
+                                        )
+
+  ## sexual behavior data
+  ba.sex.behav.data             <- subset(ego_data_ba_only,
+                                       select=c(
+                                           haveprimary,
+                                           nspnocond_mentrans.bin,
+                                           nspnocond_women.bin,
+                                           #groupsex12mo.3levels,
+                                           groupsex12mo.2levels,
+                                           bottom.condomless.sex.ind,
+                                           partner.pos.or.unknown.ind,
+                                           sexdrugs
+                                       )
+                                       )
+
+  ## social life and networks
+  ba.soc.data                    <- subset(ego_data_ba_only,
+                                        select=c(
+                                            closegay.3levels,
+                                            closeblack.3levels,
+                                            everboystown,
+                                            #memballroom,
+                                            ball.walk.binary,
+                                            memfamily,
+                                            grinderjackd,
+                                            facebook,
+                                            connected.to.prep.knower.ind,
+                                            comb.ball.gay,
+                                            recruitclose.bin
+                                        )
+                                        )
+
+    ba.intravention.data         <- subset(ego_data_ba_only,
+                                        select=c(nodrink.nodrugs.ind,
+                                            nogroupsex.recode,
+                                            en.condom.recode)
+                                        )
+
+  ## Month of interview data
+     ba.month.of.intvw              <- ego_data_ba_only$int.date.month.new
+
+
 ############################################
 
-  ## SUMMARIZE PU data
+  ## SUMMARIZE PU and BA data
      ## ses
      ## Numbers
       apply(pu.ses.data, 2, function(j)
           table(j, useNA="always")
             )
+
+      apply(ba.ses.data, 2, function(j)
+          table(j, useNA="always")
+            )
+
       ## Proportions
       apply(pu.ses.data, 2, function(j)
+          table(j, useNA="always")/sum(table(j, useNA="always"))
+            )
+
+      apply(ba.ses.data, 2, function(j)
           table(j, useNA="always")/sum(table(j, useNA="always"))
             )
 
@@ -103,8 +185,16 @@ dim(ego_data_ba_only)
       apply(pu.health.data, 2, function(j)
           table(j, useNA="always")
             )
+
+      apply(ba.health.data, 2, function(j)
+          table(j, useNA="always")
+            )
+
       ## Proportions
       apply(pu.health.data, 2, function(j)
+          table(j, useNA="always")/sum(table(j, useNA="always"))
+            )
+      apply(ba.health.data, 2, function(j)
           table(j, useNA="always")/sum(table(j, useNA="always"))
             )
 
@@ -113,8 +203,17 @@ dim(ego_data_ba_only)
       apply(pu.sex.behav.data, 2, function(j)
           table(j, useNA="always")
             )
+
+      apply(ba.sex.behav.data, 2, function(j)
+          table(j, useNA="always")
+            )
+
       ## Proportions
       apply(pu.sex.behav.data, 2, function(j)
+          table(j, useNA="always")/sum(table(j, useNA="always"))
+            )
+
+      apply(ba.sex.behav.data, 2, function(j)
           table(j, useNA="always")/sum(table(j, useNA="always"))
             )
 
@@ -123,8 +222,18 @@ dim(ego_data_ba_only)
       apply(pu.soc.data, 2, function(j)
           table(j, useNA="always")
             )
+
+      apply(ba.soc.data, 2, function(j)
+          table(j, useNA="always")
+            )
+
+
       ## Proportions
       apply(pu.soc.data, 2, function(j)
+          table(j, useNA="always")/sum(table(j, useNA="always"))
+            )
+
+      apply(ba.soc.data, 2, function(j)
           table(j, useNA="always")/sum(table(j, useNA="always"))
             )
 
@@ -132,13 +241,25 @@ dim(ego_data_ba_only)
       apply(pu.intravention.data, 2, function(j)
           table(j, useNA="always")
             )
+
+      apply(ba.intravention.data, 2, function(j)
+          table(j, useNA="always")
+            )
+
       ## Proportions
       apply(pu.intravention.data, 2, function(j)
           table(j, useNA="always")/sum(table(j, useNA="always"))
             )
-     
-  ## Month of interview
+
+      apply(ba.intravention.data, 2, function(j)
+          table(j, useNA="always")/sum(table(j, useNA="always"))
+            )
+
+  ## month of interview
      ## Numbers
       table(pu.month.of.intvw, useNA="always")
+      table(ba.month.of.intvw, useNA="always")
+
       ## Proportions
       table(pu.month.of.intvw, useNA="always")/sum(table(month.of.intvw, useNA="always"))
+      table(ba.month.of.intvw, useNA="always")/sum(table(month.of.intvw, useNA="always"))
