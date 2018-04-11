77560a8dcffb1fc240206b021a83d8b7a979a05f
Aditya Khanna <khanna7@uchicago.edu>
2018-04-11 16:37:22 -0500

    remove prep chicago analysis from here since the analysis has been moved to its own file

--- descriptives-on-266-com-fb-respondents.R
+++ descriptives-on-266-com-fb-respondents.R
@@ -55,39 +55,6 @@
          w2.fb.deg.of.ba <- degree(w2.ig, v=ba.in.w2)
          summary(w2.fb.deg.of.ba)
 
-     ## For 266 nodes in common between waves 1 and 2, compute
-        ## sizes of Facebook networks (including nonrespondents)
-        ## (adapted from simon's email dated 8/6/2015)
-        com.resp.w1.ig.idx <- which(V(w1.ig)$name %in% 
-                                      V(w1.com.resp.w.w2.data.ig)$vertex.names)
-        w1.ig.266.com.ls <- incident_edges(w1.ig, com.resp.w1.ig.idx)
-        all.w1.ig.266.com.ls <- do.call(union, w1.ig.266.com.ls) 
-        w1.ig.266.com.inc.graph <- subgraph.edges(w1.ig, all.w1.ig.266.com.ls)
-        vcount(w1.ig.266.com.inc.graph); ecount(w1.ig.266.com.inc.graph)
-        
-        com.resp.w2.ig.idx <- which(V(w2.ig)$name %in% V(w1.com.resp.w.w2.data.ig)$vertex.names)
-        w2.ig.266.com.ls <- incident_edges(w2.ig, com.resp.w2.ig.idx)
-        all.w2.ig.266.com.ls <- do.call(union, w2.ig.266.com.ls)
-        w2.ig.266.com.inc.graph <- subgraph.edges(w2.ig, all.w2.ig.266.com.ls)
-        vcount(w2.ig.266.com.inc.graph); ecount(w2.ig.266.com.inc.graph)
-  
-        ## Common edges between waves 1 and 2
-        is.directed(w1.ig.266.com.inc.graph)
-        is.directed(w2.ig.266.com.inc.graph) 
-     
-        w1.ig.266.com.inc.graph.el <- as_edgelist(w1.ig.266.com.inc.graph)
-        w2.ig.266.com.inc.graph.el <- as_edgelist(w2.ig.266.com.inc.graph)
-  
-        dim(w1.ig.266.com.inc.graph.el); dim(w2.ig.266.com.inc.graph.el)
-        
-        com.els <- rbind(w1.ig.266.com.inc.graph.el, w1.ig.266.com.inc.graph.el)
-        nrow(com.els) == nrow(w1.ig.266.com.inc.graph.el) + nrow(w2.ig.266.com.inc.graph.el)     
-  
-        dim(com.els[duplicated(com.els), ,drop = FALSE])
-        nrow(w1.ig.266.com.inc.graph.el) == nrow(com.els[duplicated(com.els), ,drop = FALSE])
-        
-        nrow(w2.ig.266.com.inc.graph.el) - nrow(w1.ig.266.com.inc.graph.el)
-  
   ## save
   save.image(file="descriptes-on-266-com-fb-respondents.RData")
         
